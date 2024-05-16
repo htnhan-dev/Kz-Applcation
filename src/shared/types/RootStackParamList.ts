99f78f6be600fc0fbf075ce0f8ef0f2477aae125
HT-Nhan <htnhan.dev@gmail.com>
2024-05-16 16:22:31 +0700

    feat: create constant variants

---
+++ src/shared/types/RootStackParamList.ts
@@ -0,0 +1,22 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
+export type RootStackParamList = {
+  Welcome: undefined;
+  Login: undefined;
+  Register: undefined;
+};
+
+export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
+  NativeStackScreenProps<RootStackParamList, Screen>;
